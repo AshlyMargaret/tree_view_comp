c8a62d71f1c424a70064ea8e02b66c50eb48f061
Ashly Margaret <ashlymargaret030@gmail.com>
2022-12-28 12:08:12 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import FileSystemNavigator from './FileSystemNavigator';
+import { Routes,Route } from "react-router-dom";
+import Sample from './Sample'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">     
+      <Routes>
+      <Route path="/" element={<FileSystemNavigator/>}/>
+      <Route path="sample" element={<Sample/>}/>
+      </Routes>
     </div>
   );
 }
---
+++ src/Sample.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Sample() {
+  return (
+    <div>
+        <h1>Hiiiii....I am sample</h1>
+    </div>
+  )
+}
+
+export default Sample
